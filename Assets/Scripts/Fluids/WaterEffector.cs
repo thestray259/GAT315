2757a3480eb336aa81021aa11afbfe8194336aa7
Tobie Allen <tallen@student.neumont.edu>
2022-05-25 11:56:37 -0600

    Water stuff started

---
+++ Assets/Scripts/Fluids/WaterEffector.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterEffector : MonoBehaviour
+{
+	[SerializeField][Range(-10, 10)] float strength = 5;
+	public Water water = null;
+
+	void Update()
+	{
+		if (Input.GetButtonDown("Fire1"))
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			water.Touch(ray, strength);
+		}
+	}
+}
